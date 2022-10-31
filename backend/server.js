1cca3402c91386fe6caf90f5cc936ed549f70151
Murthy <naaveen2@gmail.com>
2022-10-31 13:33:21 +0530

    Final deployment changes

--- backend/server.js
+++ backend/server.js
@@ -37,5 +37,5 @@ app.get('*', (req,res) => {
 })
 
 app.listen(process.env.PORT || port, () =>{
-  console.log("Express server  running on port" + config.env.port)
+  console.log("Express server  running on port" + process.env.PORT || port)
 });
